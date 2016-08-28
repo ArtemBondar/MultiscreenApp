f91736b60277c1b317b94cedcf6b27614ec93396
Artem Bondar <bondaraw@gmail.com>
2016-08-28 12:15:49 +0300

    Added audio files and implemented logic for play audio when user touch each text view;

--- app/src/main/java/com/example/android/miwok/ColorsActivity.java
+++ app/src/main/java/com/example/android/miwok/ColorsActivity.java
@@ -1,31 +1,45 @@
 package com.example.android.miwok;
 
+import android.media.MediaPlayer;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.AdapterView;
 import android.widget.ListView;
 
 import java.util.ArrayList;
 
 public class ColorsActivity extends AppCompatActivity {
-
+    private MediaPlayer mediaPlayer;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.word_list);
-        ArrayList<Word> words = new ArrayList<>();
-        words.add(new Word("wetetti", "red", R.drawable.color_red));
-        words.add(new Word("chiwiitә", "mustard yellow", R.drawable.color_mustard_yellow));
-        words.add(new Word("topiisә", "dusty yellow", R.drawable.color_dusty_yellow));
-        words.add(new Word("chokokki", "green", R.drawable.color_green));
-        words.add(new Word("takaakki", "brown", R.drawable.color_brown));
-        words.add(new Word("topoppi", "gray", R.drawable.color_gray));
-        words.add(new Word("kululli", "black", R.drawable.color_black));
-        words.add(new Word("kelelli", "white", R.drawable.color_white));
+        final ArrayList<Word> words = new ArrayList<>();
+        words.add(new Word("wetetti", "red", R.drawable.color_red, R.raw.color_red));
+        words.add(new Word("chiwiitә", "mustard yellow", R.drawable.color_mustard_yellow, R.raw.color_mustard_yellow));
+        words.add(new Word("topiisә", "dusty yellow", R.drawable.color_dusty_yellow, R.raw.color_dusty_yellow));
+        words.add(new Word("chokokki", "green", R.drawable.color_green, R.raw.color_green));
+        words.add(new Word("takaakki", "brown", R.drawable.color_brown, R.raw.color_brown));
+        words.add(new Word("topoppi", "gray", R.drawable.color_gray, R.raw.color_gray));
+        words.add(new Word("kululli", "black", R.drawable.color_black, R.raw.color_black));
+        words.add(new Word("kelelli", "white", R.drawable.color_white, R.raw.color_white));
 
         WordAdapter wordAdapter = new WordAdapter(this, words, R.color.category_colors);
 
         ListView listView = (ListView) findViewById(R.id.list);
 
         listView.setAdapter(wordAdapter);
+
+        listView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            @Override
+            public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
+                Word word = words.get(position);
+                Log.v("ColorsActivity",word.toString());
+                mediaPlayer = MediaPlayer.create(ColorsActivity.this, word.getAudioResourceID());
+                mediaPlayer.start();
+            }
+        });
     }
 }
--- app/src/main/java/com/example/android/miwok/Word.java
+++ app/src/main/java/com/example/android/miwok/Word.java
@@ -1,22 +1,26 @@
 package com.example.android.miwok;
 
 public class Word {
+
     private String miwokTranslation;
     private String defaultTranslation;
     private int imageResourceID = NO_IMAGE_PROVIDED;
     private final static int NO_IMAGE_PROVIDED = -1;
+    private int audioResourceID;
 
-    public Word(String miwokTranslation, String defaultTranslation) {
+    public Word(String miwokTranslation, String defaultTranslation, int audioResourceID) {
         this.defaultTranslation = defaultTranslation;
         this.miwokTranslation = miwokTranslation;
+        this.audioResourceID = audioResourceID;
     }
 
-    public Word(String miwokTranslation, String defaultTranslation, int imageResourceID) {
-        this.defaultTranslation = defaultTranslation;
+    public Word(String miwokTranslation, String defaultTranslation, int imageResourceID, int audioResourceID) {
+
         this.miwokTranslation = miwokTranslation;
+        this.defaultTranslation = defaultTranslation;
         this.imageResourceID = imageResourceID;
+        this.audioResourceID = audioResourceID;
     }
-
 
     public String getMiwokTranslation() {
         return miwokTranslation;
@@ -30,8 +34,21 @@ public class Word {
         return imageResourceID;
     }
 
+    public int getAudioResourceID() {
+        return audioResourceID;
+    }
+
     public boolean hasImage() {
         return imageResourceID != NO_IMAGE_PROVIDED;
     }
 
+    @Override
+    public String toString() {
+        return "Word{" +
+                "miwokTranslation='" + miwokTranslation + '\'' +
+                ", defaultTranslation='" + defaultTranslation + '\'' +
+                ", imageResourceID=" + imageResourceID +
+                ", audioResourceID=" + audioResourceID +
+                '}';
+    }
 }
