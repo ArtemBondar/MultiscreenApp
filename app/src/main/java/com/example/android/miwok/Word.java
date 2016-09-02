077f22ae41ddc0d33e0c83a2ec37a49945f17cf6
Artem Bondar <bondaraw@gmail.com>
2016-09-02 22:20:06 +0300

    Added commentaries;

--- app/src/main/java/com/example/android/miwok/Word.java
+++ app/src/main/java/com/example/android/miwok/Word.java
@@ -1,19 +1,54 @@
 package com.example.android.miwok;
 
+/**
+ * {@link Word}represents a vocabulary word that the user wants to learn.
+ * It contains a default translation,a Miwok translation,and an image for that word.
+ */
 public class Word {
-
+    /**
+     * Miwok translation for the word
+     */
     private String miwokTranslation;
+    /**
+     * Default translation for the word
+     */
     private String defaultTranslation;
+    /**
+     * Image resource ID for the word
+     */
     private int imageResourceID = NO_IMAGE_PROVIDED;
+    /**
+     * Constant value that represents no image was provided for this word
+     */
     private final static int NO_IMAGE_PROVIDED = -1;
+    /**
+     * Audio resource ID for the word
+     */
     private int audioResourceID;
 
+    /**
+     * Create a new Word object.
+     *
+     * @param defaultTranslation is the word in a language that the user is already familiar with
+     *                           (such as English)
+     * @param miwokTranslation   is the word in the Miwok language
+     * @param audioResourceID    is the resource ID for the audio file associated with this word
+     */
     public Word(String miwokTranslation, String defaultTranslation, int audioResourceID) {
         this.defaultTranslation = defaultTranslation;
         this.miwokTranslation = miwokTranslation;
         this.audioResourceID = audioResourceID;
     }
 
+    /**
+     * Create a new Word object.
+     *
+     * @param defaultTranslation is the word in a language that the user is already familiar with
+     *                           (such as English)
+     * @param miwokTranslation   is the word in the Miwok language
+     * @param imageResourceID    is the drawable resource ID for the image associated with the word
+     * @param audioResourceID    is the resource ID for the audio file associated with this word
+     */
     public Word(String miwokTranslation, String defaultTranslation, int imageResourceID, int audioResourceID) {
 
         this.miwokTranslation = miwokTranslation;
@@ -22,22 +57,37 @@ public class Word {
         this.audioResourceID = audioResourceID;
     }
 
+    /**
+     * Get the Miwok translation of the word.
+     */
     public String getMiwokTranslation() {
         return miwokTranslation;
     }
 
+    /**
+     * Get the default translation of the word.
+     */
     public String getDefaultTranslation() {
         return defaultTranslation;
     }
 
+    /**
+     * Return the image resource ID of the word.
+     */
     public int getImageResourceID() {
         return imageResourceID;
     }
 
+    /**
+     * Return the audio resource ID of the word.
+     */
     public int getAudioResourceID() {
         return audioResourceID;
     }
 
+    /**
+     * Returns whether or not there is an image for this word.
+     */
     public boolean hasImage() {
         return imageResourceID != NO_IMAGE_PROVIDED;
     }
--- app/src/main/java/com/example/android/miwok/WordAdapter.java
+++ app/src/main/java/com/example/android/miwok/WordAdapter.java
@@ -11,27 +11,24 @@ import android.widget.TextView;
 
 import java.util.ArrayList;
 
-
+/**
+ * {@link WordAdapter} is an {@link ArrayAdapter} that can provide the layout for each list item
+ * based on a data source, which is a list of {@link Word} objects.
+ */
 public class WordAdapter extends ArrayAdapter<Word> {
-
+    /**
+     * Resource ID for the background color for this list of words
+     */
     private int colorResourceId;
 
+    
     /**
-     * This is our own custom constructor (it doesn't mirror a superclass constructor).
-     * The context is used to inflate the layout file, and the list is the data we want
-     * to populate into the lists.
+     * Create a new {@link WordAdapter} object.
      *
-     * @param context The current context. Used to inflate the layout file.
-     * @param words   A List of Word objects to display in a list
+     * @param context is the current context (i.e. Activity) that the adapter is being created in.
+     * @param words is the list of {@link Word}s to be displayed.
+     * @param colorResourceId is the resource ID for the background color for this list of words
      */
-    public WordAdapter(Activity context, ArrayList<Word> words) {
-        // Here, we initialize the ArrayAdapter's internal storage for the context and the list.
-        // the second argument is used when the ArrayAdapter is populating a single TextView.
-        // Because this is a custom adapter for two TextViews and an ImageView, the adapter is not
-        // going to use this second argument, so it can be any value. Here, we used 0.
-        super(context, 0, words);
-    }
-
     public WordAdapter(Activity context, ArrayList<Word> words, int colorResourceId) {
         // Here, we initialize the ArrayAdapter's internal storage for the context and the list.
         // the second argument is used when the ArrayAdapter is populating a single TextView.
